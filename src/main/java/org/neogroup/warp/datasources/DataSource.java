398bab8a25bb7a52967fc4955f158c674acd9ad7
Luis Amengual <luis.amengual@sitrack.com>
2017-11-06 14:09:19 -0300

    Cambios menores

--- src/main/java/org/neogroup/warp/datasources/DataSource.java
+++
@@ -1,6 +0,0 @@
-package org.neogroup.warp.datasources;
-
-public class DataSource {
-
-
-}
--- src/main/java/org/neogroup/warp/datasources/DataSources.java
+++ src/main/java/org/neogroup/warp/datasources/DataSources.java
@@ -1,9 +1,14 @@
 package org.neogroup.warp.datasources;
 
+import org.neogroup.warp.Warp;
+
+import javax.sql.DataSource;
 import java.util.HashMap;
 import java.util.Map;
 
 public class DataSources {
+
+    private static final String DATA_SOURCES_DEFAULT_DATA_SOURCE_PROPERTY_NAME = "datasources.default.datasource.name";
 
     private final Map<Class, DataSource> dataSources;
     private final Map<String, DataSource> dataSourcesByName;
@@ -21,5 +26,32 @@ public class DataSources {
         if (dataSourceComponent != null) {
             dataSourcesByName.put(dataSourceComponent.name(), dataSource);
         }
+    }
+
+    public DataSource getDataSource(String dataSourceName) {
+
+        return dataSourcesByName.get(dataSourceName);
+    }
+
+    public DataSource getDataSource(Class<? extends DataSource> dataSourceClass) {
+        return dataSources.get(dataSourceClass);
+    }
+
+    public DataSource getDefaultDataSource() {
+
+        DataSource dataSource = null;
+        if (dataSources.isEmpty()) {
+            throw new RuntimeException("No data sourcess where registered !!");
+        }
+        else if (dataSources.size() == 1) {
+            dataSource = dataSources.values().iterator().next();
+        }
+        else if (Warp.hasProperty(DATA_SOURCES_DEFAULT_DATA_SOURCE_PROPERTY_NAME)) {
+            dataSource = getDataSource((String)Warp.getProperty(DATA_SOURCES_DEFAULT_DATA_SOURCE_PROPERTY_NAME));
+        }
+        else {
+            throw new RuntimeException ("Default data source property \"" + DATA_SOURCES_DEFAULT_DATA_SOURCE_PROPERTY_NAME + "\" not set !!");
+        }
+        return dataSource;
     }
 }
