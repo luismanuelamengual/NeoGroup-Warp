280b095caceca1156e7d0c58d3e08161bc19719d
Luis Amengual <luis.amengual@sitrack.com>
2017-12-27 16:02:32 -0300

    Agrego executeQuery trayendo resultados de clase

--- src/main/java/org/neogroup/warp/data/DataConnection.java
+++ src/main/java/org/neogroup/warp/data/DataConnection.java
@@ -1,5 +1,6 @@
 package org.neogroup.warp.data;
 
+import java.lang.reflect.Field;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -23,19 +24,55 @@ public abstract class DataConnection {
             ResultSet resultSet = statement.executeQuery();
             ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
             int columnCount = resultSetMetaData.getColumnCount();
-            List<DataObject> dataObjects = new ArrayList<>();
+            List<DataObject> results = new ArrayList<>();
             while (resultSet.next()) {
-                DataObject dataObject = new DataObject();
+                DataObject result = new DataObject();
                 for (int column = 1; column <= columnCount; column++) {
                     String columnName = resultSetMetaData.getColumnName(column);
                     Object value = resultSet.getObject(column);
-                    dataObject.set(columnName, value);
+                    result.set(columnName, value);
                 }
-                dataObjects.add(dataObject);
+                results.add(result);
             }
-            return dataObjects;
+            return results;
         }
         catch (SQLException exception) {
+            throw new DataException(exception);
+        }
+    }
+
+    public <T extends Object> List<T> executeQuery (Class<T> resultType, String sql, Object... parameters) {
+
+        try (Connection connection = getConnection()) {
+
+            PreparedStatement statement = connection.prepareStatement(sql);
+            if (parameters.length > 0) {
+                int parameterIndex = 1;
+                for (Object parameter : parameters) {
+                    statement.setObject(parameterIndex++, parameter);
+                }
+            }
+
+            ResultSet resultSet = statement.executeQuery();
+            ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+            int columnCount = resultSetMetaData.getColumnCount();
+            List<T> results = new ArrayList<>();
+            while (resultSet.next()) {
+                T result = resultType.getConstructor().newInstance();
+                for (int column = 1; column <= columnCount; column++) {
+                    String columnName = resultSetMetaData.getColumnName(column);
+                    Object value = resultSet.getObject(column);
+                    Field property = resultType.getField(columnName);
+                    if (property != null) {
+                        property.setAccessible(true);
+                        property.set(result, value);
+                    }
+                }
+                results.add(result);
+            }
+            return results;
+        }
+        catch (Exception exception) {
             throw new DataException(exception);
         }
     }
