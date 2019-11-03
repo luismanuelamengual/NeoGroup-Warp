96fdad127634a4e089c0b9d2de616e519d98c009
luis <luismanuelamengual@gmail.com>
2019-11-03 14:56:56 -0300

    Pequeña modificación

--- src/main/java/org/neogroup/warp/data/query/parsers/DefaultQueryParser.java
+++ src/main/java/org/neogroup/warp/data/query/parsers/DefaultQueryParser.java
@@ -9,12 +9,16 @@ public class DefaultQueryParser extends QueryParser {
 
     @Override
     public Query parseQuery(String query) throws QueryParseException{
-        List<String> tokens = getQueryTokens(query);
+        List<QueryToken> tokens = getQueryTokens(query);
         if (tokens.isEmpty()) {
             throw new QueryParseException("No tokens found in query");
         }
+        QueryToken firstToken = tokens.get(0);
+        if (firstToken.getType() != QueryTokenType.WORD) {
+            throw new QueryParseException("Unexpected query token \"" + firstToken.getValue() + "\"");
+        }
         Query parsedQuery = null;
-        switch (tokens.get(0)) {
+        switch (firstToken.getValue().toString()) {
             case QueryStatement.SELECT:
                 parsedQuery = createSelectQuery(tokens);
                 break;
@@ -31,24 +35,55 @@ public class DefaultQueryParser extends QueryParser {
         return parsedQuery;
     }
 
-    private SelectQuery createSelectQuery(List<String> tokens) {
+    private SelectQuery createSelectQuery(List<QueryToken> tokens) {
         return null;
     }
 
-    private InsertQuery createInsertQuery(List<String> tokens) {
+    private InsertQuery createInsertQuery(List<QueryToken> tokens) {
         throw new UnsupportedOperationException();
     }
 
-    private UpdateQuery createUpdateQuery(List<String> tokens) {
+    private UpdateQuery createUpdateQuery(List<QueryToken> tokens) {
         throw new UnsupportedOperationException();
     }
 
-    private DeleteQuery createDeleteQuery(List<String> tokens) {
+    private DeleteQuery createDeleteQuery(List<QueryToken> tokens) {
         throw new UnsupportedOperationException();
     }
 
-    private List<String> getQueryTokens(String query) {
-        List<String> tokens = new ArrayList<>();
+    private enum QueryTokenType {
+        WORD,
+        VALUE,
+        OPENING_PARENTHESIS,
+        CLOSING_PARENTHESIS,
+        COMMA
+    }
+
+    private class QueryToken {
+        private final QueryTokenType type;
+        private final Object value;
+
+        public QueryToken(QueryTokenType type, Object value) {
+            this.type = type;
+            this.value = value;
+        }
+
+        public QueryTokenType getType() {
+            return type;
+        }
+
+        public Object getValue() {
+            return value;
+        }
+
+        @Override
+        public String toString() {
+            return value.toString();
+        }
+    }
+
+    private List<QueryToken> getQueryTokens(String query) {
+        List<QueryToken> tokens = new ArrayList<>();
         StringBuilder token = null;
         int pos = 0;
         char tokenQuotationCharacter = 0;
@@ -63,7 +98,7 @@ public class DefaultQueryParser extends QueryParser {
                     }
                     else {
                         if (character == tokenQuotationCharacter) {
-                            tokens.add(token.toString());
+                            tokens.add(new QueryToken(QueryTokenType.VALUE, token.toString()));
                             token = null;
                             tokenQuotationCharacter = 0;
                             pos++;
@@ -76,7 +111,13 @@ public class DefaultQueryParser extends QueryParser {
                 }
                 else {
                     if (Character.isWhitespace(character) || character == QueryStatement.COMMA ||character == QueryStatement.OPENING_PARENTHESIS || character == QueryStatement.CLOSING_PARENTHESIS) {
-                        tokens.add(token.toString());
+                        String tokenValueStr = token.toString();
+                        try {
+                            tokens.add(new QueryToken(QueryTokenType.VALUE, Double.parseDouble(tokenValueStr)));
+                        }
+                        catch (Throwable ex) {
+                            tokens.add(new QueryToken(QueryTokenType.WORD, tokenValueStr));
+                        }
                         token = null;
                     }
                     else {
@@ -87,8 +128,12 @@ public class DefaultQueryParser extends QueryParser {
 
             if (token == null) {
                 if (!Character.isWhitespace(character)) {
-                    if (character == QueryStatement.OPENING_PARENTHESIS || character == QueryStatement.CLOSING_PARENTHESIS || character == QueryStatement.COMMA) {
-                        tokens.add(String.valueOf(character));
+                    if (character == QueryStatement.OPENING_PARENTHESIS) {
+                        tokens.add(new QueryToken(QueryTokenType.OPENING_PARENTHESIS, String.valueOf(character)));
+                    } else if (character == QueryStatement.CLOSING_PARENTHESIS) {
+                        tokens.add(new QueryToken(QueryTokenType.CLOSING_PARENTHESIS, String.valueOf(character)));
+                    } else if (character == QueryStatement.COMMA) {
+                        tokens.add(new QueryToken(QueryTokenType.COMMA, String.valueOf(character)));
                     } else {
                         token = new StringBuilder();
                         if (character == QueryStatement.QUOTATION_MARK || character == QueryStatement.DOUBLE_QUOTATION_MARK) {
@@ -102,8 +147,14 @@ public class DefaultQueryParser extends QueryParser {
             }
             pos++;
         }
-        if (token != null) {
-            tokens.add(token.toString());
+        if (token != null && tokenQuotationCharacter == 0) {
+            String tokenValueStr = token.toString();
+            try {
+                tokens.add(new QueryToken(QueryTokenType.VALUE, Double.parseDouble(tokenValueStr)));
+            }
+            catch (Throwable ex) {
+                tokens.add(new QueryToken(QueryTokenType.WORD, tokenValueStr));
+            }
         }
         return tokens;
     }
